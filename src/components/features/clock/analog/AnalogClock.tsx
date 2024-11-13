99c0c6c3dcc1ffb177e541ce4caae9056907e72e
Ryan_Ray_Official <rluke7734@gmail.com>
2024-11-13 03:26:33 -0500

    Remove Old React import from Components

--- src/components/features/clock/analog/AnalogClock.tsx
+++ src/components/features/clock/analog/AnalogClock.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Card } from '@/components/ui/card';
 import { useClockStore } from '@/store/clock';
 import { motion } from 'framer-motion';
